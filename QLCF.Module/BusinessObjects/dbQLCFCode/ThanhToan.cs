37d55e6dd29777aef0fc8f53ed9a7fd4e441b2d6
Nguyen huu thien <huuthien034501@gmail.com>
2024-06-24 18:20:41 +0700

    Add project files.

---
+++ QLCF.Module/BusinessObjects/dbQLCFCode/ThanhToan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace QLCF.Module.dbQLCF
+{
+    [DefaultClassOptions]
+    [NavigationItem]
+    public partial class ThanhToan
+    {
+        public ThanhToan(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
